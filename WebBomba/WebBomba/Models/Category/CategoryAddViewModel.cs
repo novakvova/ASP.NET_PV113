dca8431801e6d6f8927b722b6933d240b0be51fd
novakvova <novakvova@gmail>
2023-09-14 20:47:44 +0300

    Auto generation View

--- WebBomba/WebBomba/Models/Category/CategoryAddViewModel.cs
+++ WebBomba/WebBomba/Models/Category/CategoryAddViewModel.cs
@@ -4,9 +4,14 @@ namespace WebBomba.Models.Category
 {
     public class CategoryAddViewModel
     {
+        [Display(Name="Назва")]
         [Required(ErrorMessage = "Вкажіть назву категорії")]
         public string Name { get; set; }
+        [Display(Name="Опис")]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+        [Display(Name="Фото")]
+        [DataType(DataType.Upload)]
         public IFormFile Image { get; set; }
     }
 }
