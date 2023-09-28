ad5ae483b788bfd1b09fec07bc8260f1f48b1ad1
novakvova <novakvova@gmail>
2023-09-28 19:27:03 +0300

    Basic add product form

---
+++ WebBomba/WebBomba/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebBomba.Data;
+using WebBomba.Interfaces;
+using WebBomba.Models.Product;
+
+namespace WebBomba.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly DataEFContext _dataEFContext;
+
+        public ProductController(DataEFContext dataEFContext)
+        {
+            _dataEFContext = dataEFContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var categories = _dataEFContext.Categories
+                .Select(x=> new {Value = x.Id, Text = x.Name})
+                .ToList();
+            var model = new ProductAddViewModel
+            {
+                CategoryList = new SelectList(categories, "Value", "Text")
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ WebBomba/WebBomba/Models/Product/ProductAddViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebBomba.Models.Product
+{
+    public class ProductAddViewModel
+    {
+            [Display(Name = "Назва")]
+            [Required(ErrorMessage = "Вкажіть назву продукту")]
+            [StringLength(500, ErrorMessage = "Назва продукту не повинна перевищувати 500 символів")]
+            public string Name { get; set; }
+
+            [Display(Name = "Опис")]
+            [StringLength(4000, ErrorMessage = "Опис продукту не повинен перевищувати 4000 символів")]
+            public string Description { get; set; }
+
+            [Display(Name = "Категорія")]
+            [Required(ErrorMessage = "Оберіть категорію")]
+            public int CategoryId { get; set; }
+
+            [Display(Name = "Фото")]
+            [DataType(DataType.Upload)]
+            public IFormFileCollection Images { get; set; }
+
+            //список усіх категорій, які відобрається для продукта
+            public SelectList CategoryList { get; set; }
+    }
+}
