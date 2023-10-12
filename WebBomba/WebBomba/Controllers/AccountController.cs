cc8c5b81abf982ca368ecc0fa1c4d7a71d819006
novakvova <novakvova@gmail>
2023-10-12 20:50:41 +0300

    Login is good

---
+++ WebBomba/WebBomba/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebBomba.Data.Entities.Identity;
+using WebBomba.Models.Account;
+
+namespace WebBomba.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<UserEntity> _userManager;
+        private readonly SignInManager<UserEntity> _signInManager;
+
+        public AccountController(UserManager<UserEntity> userManager, 
+            SignInManager<UserEntity> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user != null)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return Redirect("/");
+                    }
+                }
+            }
+
+            ModelState.AddModelError("", "Дані вказано не вірно");
+            return View(model);
+        }
+    }
+}
---
+++ WebBomba/WebBomba/Models/Account/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebBomba.Models.Account
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Електронна пошта")]
+        [Required(ErrorMessage = "Вкажіть пошту")]
+        public string Email { get; set; }
+        [Display(Name = "Вкажіть пароль")]
+        [Required(ErrorMessage = "Вкажіть пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
