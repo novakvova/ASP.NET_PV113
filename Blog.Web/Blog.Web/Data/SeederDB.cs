4eecba08af8ac1496df5a5cc2a9f1e1a4a21d1fe
novakvova <novakvova@gmail.com>
2024-03-02 15:54:17 +0200

    Add Seed Categories

--- Blog.Web/Blog.Web/Data/SeederDB.cs
+++ Blog.Web/Blog.Web/Data/SeederDB.cs
@@ -1,5 +1,8 @@
 ﻿using Blog.Web.Constants;
+using Blog.Web.Data.Entities;
 using Blog.Web.Data.Entities.Identity;
+using Blog.Web.Helpers;
+using Bogus;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,7 +24,6 @@ namespace Blog.Web.Data
 
                 var roleManager = scope.ServiceProvider
                     .GetRequiredService<RoleManager<RoleEntity>>();
-
 
                 #region Додавання користувачів та ролей
 
@@ -50,6 +52,28 @@ namespace Blog.Web.Data
                     {
                         result = userManager.AddToRoleAsync(user, Roles.Admin).Result;
                     }
+                }
+
+                #endregion
+
+                #region Додаємо категорії
+
+                if (!context.Categories.Any())
+                {
+                    var fakeCategory = new Faker<CategoryEntity>()
+                        .RuleFor(c => c.Name, f => f.Commerce.Categories(1)[0])
+                        .RuleFor(c => c.UrlSlug, (f, c) => UrlSlugMaker.GenerateSlug(c.Name))
+                        .RuleFor(c => c.Description, f => f.Lorem.Sentence());
+
+                    List<CategoryEntity> fakeCategoryData = fakeCategory.Generate(10);
+
+                    fakeCategoryData = fakeCategoryData
+                        .GroupBy(category => category.Name)
+                        .Select(group => group.First())
+                        .ToList();
+
+                    context.Categories.AddRange(fakeCategoryData);
+                    context.SaveChanges();
                 }
 
                 #endregion
---
+++ Blog.Web/Blog.Web/Helpers/UrlSlugMaker.cs
@@ -0,0 +1,20 @@
+﻿namespace Blog.Web.Helpers
+{
+    public static class UrlSlugMaker
+    {
+        public static string GenerateSlug(string text)
+        {
+            string urlSlug = text.ToLowerInvariant();
+
+            char[] invalidChars = { '.', ',', ':', ';' };
+            foreach (char invalidChar in invalidChars)
+            {
+                urlSlug = urlSlug.Replace(invalidChar.ToString(), string.Empty);
+            }
+
+            urlSlug = urlSlug.Replace(" ", "_");
+
+            return urlSlug;
+        }
+    }
+}
